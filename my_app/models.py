ee915c1fa6579763fbbdfa5c1c6681e7d5f0411e
dipankarsahoo <dsahooa56@gmail.com>
2023-04-21 16:50:21 +0530

    Admin changes

--- my_app/models.py
+++ my_app/models.py
@@ -1,18 +1,20 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+from model_utils import Choices
 
 # Create your models here.
 class Person(models.Model):
-    GENDER = [
-        ("M", "Male"),
-        ("F", "Female"),
-        ("O", "Other"),
-    ]
+    GENDER = Choices(
+        (1, "male", _("male")),
+        (2, "female", _("female")),
+        (3, "others", _("others")),
+    )
     
     fname = models.CharField(max_length=50)
     lname = models.CharField(max_length=50)
     age = models.IntegerField(default=0, null=True)
     email = models.EmailField()
-    gender = models.CharField(max_length=1, choices=GENDER, default=GENDER[2])
+    gender = models.IntegerField(choices=GENDER, default=GENDER.male)
     
     class Meta:
         db_table = "person"
