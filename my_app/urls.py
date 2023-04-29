ef0b851c658540f60453e065ba4a8d3f1377d4cb
dipankarsahoo <dsahooa56@gmail.com>
2023-04-29 18:49:29 +0530

    templates

--- my_app/urls.py
+++ my_app/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
-from my_app.views import hi_world, hello_world
+from my_app.views import hi_world, hello_world, person_info, person_search
 
 urlpatterns = [
     path('hi', hi_world),
     path('hello_world', hello_world),
+    path('person-info/<int:person_id>', person_info),
+    path('person-search/<str:person_name>', person_search)
 ]
--- my_app/views.py
+++ my_app/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse 
 from django.shortcuts import render
+from my_app.models import Person
 import datetime
 
 # Create your views here.
@@ -10,4 +11,33 @@ def hi_world(request):
     return HttpResponse(html)
 
 def hello_world(request):
-    return render(request, "hello_world.html")
+    my_data = {
+        "name": "Dipankar",
+        "tech_stack": "Python",
+        "role": "Software developer"
+    }
+    return render(request, "hello_world.html", context= my_data)
+
+def person_info(request, person_id):
+    print('person_id in view ...', person_id)
+    person_object = Person.objects.get(id=person_id)
+    my_data = {
+        "name": person_object.fname + " " + person_object.lname,
+        "email": person_object.email,
+        "gender": person_object.gender
+    }
+    return render(request, "person_info.html", context= my_data)
+
+def person_search(request, person_name):
+    print('person_id in view ...', person_name)
+    try:
+        person_object = Person.objects.filter(fname= person_name).first()
+    except Person.DoesNotExist:
+        # handle the case where no matching objects are found
+        person_object = None
+    my_data = {
+        "name": person_object.fname + " " + person_object.lname,
+        "email": person_object.email,
+        "gender": person_object.gender
+    }
+    return render(request, "person_search.html", context= my_data)
